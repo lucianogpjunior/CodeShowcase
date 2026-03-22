bc94b0c0eb9a874a28456c9f395da5169611b8b7
lucianogpjr <lucianojunior4801@gmail.com>
2026-03-22 20:24:03 -0300

    NavBar, title no Index, main e app

--- front/src/main.js
+++ front/src/main.js
@@ -1,12 +1,24 @@
 import Vue from 'vue';
 import App from './App.vue';
-import { createPinia } from "pinia";
 
-const pinia = createPinia()
+// Importa Pinia
+import { createPinia, PiniaVuePlugin } from 'pinia';
 
-Vue.config.productionTip = false
+// Importa BootstrapVue
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// Configurações
+Vue.use(PiniaVuePlugin);
+const pinia = createPinia();
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+Vue.config.productionTip = false;
 
 new Vue({
   pinia,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
